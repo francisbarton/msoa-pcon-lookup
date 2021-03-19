60cf69caa4a09de23cf7fb6628d4e57a8417c9d5
Francis Barton <fbarton@alwaysdata.net>
2021-03-19 03:53:33 +0000

    make boundaries and joins

--- README.md
+++ README.md
@@ -3,13 +3,15 @@
 
 (England and Wales only)
 
-## Premises:
+### Some premises:
 
-a. All Wards nest completely within LA boundaries
-b. All MSOAs nest completely within LA boundaries
-c. (alt to a and b: no Ward or MSOA is split between more than 1 LA)
-d. All Wards nest completely within Constituency boundaries
-e. However, many MSOAs _do not_ nest within Constituency boundaries
+* All Wards nest completely within LA boundaries
+* All MSOAs nest completely within LA boundaries
+* (alt to a and b: no Ward or MSOA is split between more than 1 LA)
+* All Wards nest completely within Constituency boundaries
+* However, many MSOAs _do not_ nest within Constituency boundaries
+
+### Motivation
 
 [The House of Commons Library][hocl-bb] provides Constituency:MSOA data downloads, [for example for broadband data (xlsx link)][hocl-xl].
 
@@ -21,12 +23,24 @@ This provides an excellent _constituency-centric_ interface to all sorts of data
 
 
 But if you want to access their datasets from the perspective of a set of MSOAs within an LA, it's harder to know how to get the data you want.
-Many MSOAs contain part of more than 1 (but never, I think, more than 2) constituenc[y|ies].
+Many MSOAs contain part of more than 1 constituency*.
+
+
+\* I suspected no MSOA contained part of more than 2 constituencies, but there is at least one that does: E02000217 Croydon Minster & Waddon North contains parts of Croydon Central, Croydon North and Croydon South.
+There may be others.
+
+E02000291 Enfield Town South & Bush Hill Park contains parts of Edmonton, Enfield North and Enfield, Southgate.
+
+E02001370 Everton East: Liverpool, Walton; Liverpool, Wavertree; Liverpool, West Derby.
+
+E02006852 Far Headingley & Weetwood: Leeds North East, Leeds North West and Leeds West.
+
+OK there's quite a few.
 
 This repo aims to provide a lookup for MSOA to LA to Constituency to Region.
 
-(An aside: I wonder if, or how many, constituencies cross a regional border.
-I kind of hope that none does, but it's certainly possible in theory — I'm pretty sure they can cross upper tier authority boundaries.)
+_(An aside: I wonder if, or how many, constituencies cross a regional border.
+I kind of hope that none does, but it's certainly possible in theory — I'm pretty sure they can cross upper tier authority boundaries.)_
 
 The lookup will provide a % figure for the area of an MSOA that is within any constituency it overlaps.
 This will enable a best-fit approach — where an MSOA is allocated to a single constituency which has the greatest part of its area — or alternatively the filtering out of small % overlaps as negligible.
---
+++ data.R
@@ -0,0 +1,107 @@
+library(here)
+library(dplyr)
+library(readr)
+library(stringr)
+library(purrr)
+library(janitor)
+library(sf)
+library(jogger)
+
+
+# Source data URLs --------------------------------------------------------
+
+# https://geoportal.statistics.gov.uk/datasets/ward-to-westminster-parliamentary-constituency-to-local-authority-district-to-upper-tier-local-authority-december-2020-lookup-in-the-united-kingdom-v2/data
+pcon_lad_url <- "https://opendata.arcgis.com/datasets/063ccaa43b9a4f4281b3ad803c1ed2e8_0.csv"
+
+
+# https://geoportal.statistics.gov.uk/datasets/output-area-to-lower-layer-super-output-area-to-middle-layer-super-output-area-to-local-authority-district-december-2020-lookup-in-england-and-wales
+msoa_lad_url <- "https://opendata.arcgis.com/datasets/65664b00231444edb3f6f83c9d40591f_0.csv"
+
+
+# https://visual.parliament.uk/msoanames
+hocl_msoa_names_url <- "https://visual.parliament.uk/msoanames/static/MSOA-Names-Latest.csv"
+
+
+
+
+
+
+
+# MSOA and Constituency boundary data -------------------------------------
+
+# https://geoportal.statistics.gov.uk/datasets/westminster-parliamentary-constituencies-december-2019-boundaries-uk-bgc/
+pcon_bounds_url <- "https://opendata.arcgis.com/datasets/937997590f724a398ccc0100dbd9feee_0.geojson"
+
+
+# https://geoportal.statistics.gov.uk/datasets/middle-layer-super-output-areas-december-2011-boundaries-generalised-clipped-bgc-ew-v3
+msoa_bounds_url <- "https://opendata.arcgis.com/datasets/abfccdf1071c43dd981a49eb7da13d2b_0.geojson"
+
+
+# https://geoportal.statistics.gov.uk/datasets/middle-layer-super-output-areas-december-2011-population-weighted-centroids/
+msoa_centroids_url <- "https://opendata.arcgis.com/datasets/b0a6d8a3dc5d4718b3fd62c548d60f81_0.geojson"
+
+
+
+
+# Build MSOA table --------------------------------------------------------
+
+msoa_lad <- read_csv(msoa_lad_url) %>%
+  clean_names() %>%
+  select(starts_with(c("msoa", "lad", "rgn"))) %>%
+  distinct()
+
+msoa_names <- read_csv(hocl_msoa_names_url) %>%
+  clean_names() %>%
+  select(!laname)
+
+
+msoa_lad <- msoa_names %>%
+  left_join(msoa_lad)
+
+
+
+# Build PCON table --------------------------------------------------------
+
+pcon_lad <- read_csv(pcon_lad_url) %>%
+  clean_names() %>%
+  filter(across(wd20cd, ~ str_detect(., "^[E|W]"))) %>%
+  select(starts_with(c("pcon", "lad"))) %>%
+  distinct() %>%
+  mutate(across(pcon20nm, ~ case_when(
+    . == "Birmingham,  Selly Oak" ~ "Birmingham, Selly Oak",
+    TRUE ~ .
+  )))
+
+
+
+# Build sf (geographical) tibbles -----------------------------------------
+
+pcon_bounds <- st_read(pcon_bounds_url) %>%
+  clean_names() %>%
+  select(starts_with(c("pcon", "bng"))) %>%
+  rename_with(~ str_replace_all(., "^pcon19", "pcon20")) %>%
+  rename_with(~ str_replace_all(., "^bng", "centroid")) %>%
+  mutate(across(pcon20nm, ~ case_when(
+    . == "Weston-Super-Mare" ~ "Weston-super-Mare",
+    . == "Ynys Mon" ~ "Ynys Môn",
+    TRUE ~ .
+  ))) %>%
+  inner_join(pcon_lad) %>%
+  dplyr::relocate(starts_with("centroid"), .after = lad20nm)
+
+
+
+msoa_bounds <- st_read(msoa_bounds_url) %>%
+  clean_names() %>%
+  select(starts_with(c("msoa", "bng", "shape_area"))) %>%
+  rename_with(~ str_replace_all(., "^bng", "centroid")) %>%
+  left_join(msoa_lad) %>%
+  dplyr::relocate(starts_with(c("centroid", "shape")), .after = rgn20nm)
+
+
+msoa_centroids <- st_read(msoa_centroids_url) %>%
+  clean_names() %>%
+  select(starts_with("msoa")) %>%
+  left_join(msoa_lad)
+
+
